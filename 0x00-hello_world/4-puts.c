55ad5840e901c8b594306dbcd37c0da48618728c
Muluken Gashaw <mlkngashaw@gmail.com>
2023-02-16 07:30:48 -0800

    a C program that printsa line Using the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints a line usin the function puts
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
