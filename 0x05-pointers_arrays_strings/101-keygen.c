0caf5dd83770f6d313f4b0384191dd09a2f748d8
Muluken Gashaw <mlkngashaw@gmail.com>
2023-02-27 18:43:03 -0800

    Tada! Congrats

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,54 +3,34 @@
 #include <time.h>
 
 /**
- * main - Generates random valid passwords for the program 101-crackme.
- *
- * description: finds checksum value from objdump
+ * main - A program that generates random valid passwords for the
+ *        program 101-crackme.
  *
  * Return: 0
  */
 
-
 int main(void)
 {
-	int counter, sum, random;
+	int password[100];
+	int i, sum, n;
 
 	sum = 0;
-	counter = 0;
-	random = 0;
-
-	char password[80];
 
 	srand(time(NULL));
 
-	while (sum < 2772)
+	for (i = 0; i < 100; i++)
 	{
-
-		if (2772 - sum < 48)
+		password[i] = rand() % 78;
+		sum += (password[i] + '0');
+		putchar(password[i] + '0');
+		if ((2772 - sum) - '0' < 78)
 		{
-			sum -= password[--counter];
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
 		}
-		else if (2772 - sum <= 126)
-		{
-			random = 2772 - sum;
-		}
-		else
-		{
-			random = rand() % (126 - 48) + 48;
-		}
-
-		if (random)
-		{
-			password[counter++] = random;
-			sum += random;
-		}
-		random = 0;
 	}
-
-	password[counter] = '\0';
-	printf("%s\n", password);
 
 	return (0);
 }
-
-
