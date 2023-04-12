a140eed084d86254b58096e2fe7866e225cad6c3
Muler-g <mlkngashaw@gmail.com>
2023-04-12 04:33:28 -0700

    update; git commit -m update
    git commit -m update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,44 +11,39 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, r, w;
+	int fd_from, fd_to, from_copy, to_copy, c1, c2;
 	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	file_to = open(argv[2],  O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	if (file_to == -1)
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	/* read a copy with a buffer size of 1024 */
+	while ((from_copy = read(fd_from, buffer, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while (r == 1024)
-	{
-		r = read(file_from, buffer, 1024);
-		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		w = write(file_to, buffer, r);
-		if (w == -1)
+		to_copy = write(fd_to, buffer, from_copy);
+		if (from_copy != to_copy)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-
+	if (from_copy == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	/*clode fd */
+	c1 = close(fd_from);
+	if (c1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	c2 = close(fd_to);
+	if (c2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }
